530382e93bccaeeb73541efcb6c7ca654986ed5f
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2025-04-30 07:20:18 -0400

    Initial Commit

---
+++ AquinasAI - Latin-English Bible/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AquinasAI - Latin-English Bible
+//
+//  Created by Carson Mulligan on 4/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
