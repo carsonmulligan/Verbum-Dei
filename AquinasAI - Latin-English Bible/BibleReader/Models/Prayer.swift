d9d359ece1e425f0636beca44b432375bba033e4
Carson Mulligan <103183546+carsonmulligan@users.noreply.github.com>
2025-05-13 19:14:32 -0400

    Fix Prayer model Decodable conformance and variable mutability warning

--- AquinasAI - Latin-English Bible/BibleReader/Models/Prayer.swift
+++ AquinasAI - Latin-English Bible/BibleReader/Models/Prayer.swift
@@ -16,6 +16,26 @@ struct Prayer: Identifiable, Codable {
     var displayTitleEnglish: String {
         title_english ?? title
     }
+    
+    // Custom coding keys to match JSON structure
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case title_latin
+        case title_english
+        case latin
+        case english
+    }
+    
+    // Custom decoder to handle category which isn't in JSON
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        title = try container.decode(String.self, forKey: .title)
+        title_latin = try container.decodeIfPresent(String.self, forKey: .title_latin)
+        title_english = try container.decodeIfPresent(String.self, forKey: .title_english)
+        latin = try container.decode(String.self, forKey: .latin)
+        english = try container.decode(String.self, forKey: .english)
+        // category is set after decoding by PrayerStore
+    }
 }
 
 class PrayerStore: ObservableObject {
@@ -40,7 +60,7 @@ class PrayerStore: ObservableObject {
             if let url = Bundle.main.url(forResource: filename.replacingOccurrences(of: ".json", with: ""), withExtension: "json") {
                 do {
                     let data = try Data(contentsOf: url)
-                    var prayersContainer = try JSONDecoder().decode([String: [Prayer]].self, from: data)
+                    let prayersContainer = try JSONDecoder().decode([String: [Prayer]].self, from: data)
                     // Assign category to each prayer
                     if var categoryPrayers = prayersContainer["prayers"] {
                         for i in categoryPrayers.indices {
